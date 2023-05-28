e4e3e712ca5c3634cb52f8b2a21eba1da6067e06
xipingguo456 <94697241+xipingguo456@users.noreply.github.com>
2023-05-28 17:10:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,6 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'transitive_bs',
   github: 'transitive-bullshit',
-  link
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
