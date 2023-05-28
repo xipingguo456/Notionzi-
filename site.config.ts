e333472618ec41670115de66d2693e1071992806
xipingguo456 <94697241+xipingguo456@users.noreply.github.com>
2023-05-28 16:38:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '测试',
+  domain: 'notion4me',
+  author: 'LZY',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
